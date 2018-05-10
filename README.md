076a02f07e8b86140f16575c02ba6e37e847a0f0
Shawn Woodford <sw@shawnwoodford.com>
2018-05-10 12:39:46 -0400

    setup new repo

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# terraform
-A collection of bash shell scripts for automating various tasks with Terraform.
+<h1 align="center"><img src="/images/terraform.png" alt="Terraform" width=214 height=60></h1>
+
+<h2 align="center">A collection of bash shell scripts for automating various tasks with <a href="https://www.terraform.io/" target="_blank">Terraform</a>.</h2>
+
+[![Build Status](https://travis-ci.org/swoodford/terraform.svg?branch=master)](https://travis-ci.org/swoodford/terraform)
+
+#### [https://github.com/swoodford/terraform](https://github.com/swoodford/terraform)
+
+## Table of contents
+
+- [Getting Started](#getting-started)
+- [What's Included](#tools-included-in-this-repo)
+- [Bugs and Feature Requests](#bugs-and-feature-requests)
+- [Creator](#creator)
+- [Copyright and License](#copyright-and-license)
+
+## Getting Started
+
+### What is Terraform?
+
+Terraform is an open source tool for infrastructure as code that enables you to safely and predictably create, change, and improve infrastructure.
+
+[Installing Terraform](https://www.terraform.io/intro/getting-started/install.html)
+
+
+## Tools included in this repo:
+
+- **[terraform-aws-waf-pingdom.sh](terraform-aws-waf-pingdom.sh)** Generate a Terraform AWS WAF IPSet, WAF Rule, and WAF Web ACL resource blocks for Pingdom probe server IPs
+- **[terraform-redact-iam-secrets.sh](terraform-redact-iam-secrets.sh)** Replaces AWS IAM Secret Keys and IAM SES SMTP Passwords with "REDACTED" in Terraform state files
+
+## Bugs and feature requests
+Have a bug or a feature request? The [issue tracker](https://github.com/swoodford/terraform/issues) is the preferred channel for bug reports, feature requests and submitting pull requests.
+If your problem or idea is not addressed yet, [please open a new issue](https://github.com/swoodford/terraform/issues/new).
+
+## Creator
+
+**Shawn Woodford**
+
+- <https://shawnwoodford.com>
+- <https://github.com/swoodford>
+
+## Copyright and License
+
+Code and Documentation Copyright 2012-2018 Shawn Woodford. Code released under the [Apache License 2.0](https://github.com/swoodford/terraform/blob/master/LICENSE).
---
+++ terraform-aws-waf-pingdom.sh
@@ -0,0 +1,201 @@
+#!/usr/bin/env bash
+
+# This script will generate a Terraform AWS WAF IPSet, WAF Rule, and WAF Web ACL resource blocks
+# in a single Terraform file from a list of current Pingdom IPv4 probe server IPs.
+# This can be used to easily whitelist Pingdom probes in an AWS WAF Web ACL.
+
+# Requires jq, wget, perl
+
+# Set Variables
+FriendlyName="Allow From Pingdom"
+
+
+# Do not modify these variables!
+TerraformResourceName=$(echo "$FriendlyName" | tr '[:upper:]' '[:lower:]' | tr -s ' ' '_' | tr -s '.' '_')
+MetricName=$(echo "$FriendlyName" | sed 's/[^a-z  A-Z]//g' | tr -d '[:space:]')
+OutputFileName="aws_waf_$TerraformResourceName.tf"
+
+# Debug Mode
+DEBUGMODE="0"
+
+
+# Functions
+
+
+# Pause
+function pause(){
+	read -n 1 -s -p "Press any key to continue..."
+	echo
+}
+
+# Check Command
+function check_command(){
+	for command in "$@"
+	do
+	    type -P $command &>/dev/null || fail "Unable to find $command, please install it and run this script again."
+	done
+}
+
+# Completed
+function completed(){
+	echo
+	HorizontalRule
+	tput setaf 2; echo "Completed!" && tput sgr0
+	tput setaf 2; echo "$1" && tput sgr0
+	HorizontalRule
+	echo
+}
+
+# Fail
+function fail(){
+	tput setaf 1; echo "Error: $*" && tput sgr0
+	exit 1
+}
+
+# Horizontal Rule
+function HorizontalRule(){
+	echo "============================================================"
+}
+
+# Get Pingdom IPv4 IPs
+# https://help.pingdom.com/hc/en-us/articles/203682601-How-to-get-all-Pingdom-probes-public-IP-addresses
+function probeIPs(){
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo "function probeIPs"
+	fi
+	wget --quiet -O- https://www.pingdom.com/rss/probe_servers.xml | \
+	perl -nle 'print $1 if /IP: (([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5]));/' | \
+	sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 | \
+	uniq > pingdom-probe-servers.txt
+
+	TOTALIPS=$(cat pingdom-probe-servers.txt | wc -l | tr -d ' ')
+
+	if ! [ "$TOTALIPS" -gt "0" ]; then
+		fail "Unable to lookup Pingdom IPs."
+	fi
+	echo
+	HorizontalRule
+	echo "Total Pingdom IPs: "$TOTALIPS
+	HorizontalRule
+	echo
+}
+
+# Builds the JSON for 101-150 rules
+function buildTF(){
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo "function buildTF"
+	fi
+	echo
+	HorizontalRule
+	echo "Building Terraform Resources..."
+	HorizontalRule
+	echo
+	(
+	cat << EOP
+resource "aws_waf_ipset" "$TerraformResourceName" {
+  name = "$FriendlyName"
+
+  ip_set_descriptors = [
+EOP
+	) > temp1
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo built temp1
+	fi
+	rm -f temp2
+
+	START=1
+	for (( COUNT=$START; COUNT<=$TOTALIPS; COUNT++ ))
+	do
+	iplist=$(nl pingdom-probe-servers.txt | grep -w [^0-9][[:space:]]$COUNT | cut -f 2)
+	(
+	cat << EOP
+      { type = "IPV4", value = "$iplist/32" },
+EOP
+	) >> temp2
+	done
+
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo built temp2
+	fi
+
+	# Remove the last comma to close JSON array
+	cat temp2 | sed '$ s/.$//' > temp3
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo built temp3
+	fi
+
+	(
+	cat << 'EOP'
+    ]
+}
+
+EOP
+	) > temp4
+
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo built temp4
+	fi
+	rm -f temp5
+
+	(
+	cat << EOP
+resource "aws_waf_rule" "$TerraformResourceName" {
+  depends_on  = ["aws_waf_ipset.$TerraformResourceName"]
+  name        = "$FriendlyName"
+  metric_name = "$MetricName"
+
+  predicates {
+    data_id = "\${aws_waf_ipset.$TerraformResourceName.id}"
+    negated = false
+    type    = "IPMatch"
+  }
+}
+
+resource "aws_waf_web_acl" "$TerraformResourceName" {
+  depends_on  = ["aws_waf_ipset.$TerraformResourceName", "aws_waf_rule.$TerraformResourceName"]
+  name        = "$FriendlyName"
+  metric_name = "$MetricName"
+
+  default_action {
+    type = "BLOCK"
+  }
+
+  rules {
+    action {
+      type = "ALLOW"
+    }
+
+    priority = 1
+    rule_id  = "\${aws_waf_rule.$TerraformResourceName.id}"
+    type     = "REGULAR"
+  }
+}
+EOP
+	) >> temp5
+
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo built temp5
+	fi
+
+	cat temp1 temp3 temp4 temp5 > "$OutputFileName"
+	if [[ $DEBUGMODE = "1" ]]; then
+		echo "built $OutputFileName"
+	fi
+
+}
+
+# Run the script and call functions
+
+# Check for required applications
+check_command jq wget perl
+
+probeIPs
+
+buildTF
+
+# Cleanup temp files
+if ! [[ $DEBUGMODE = "1" ]]; then
+	rm -f temp1 temp2 temp3 temp4 temp5
+fi
+
+completed "Terraform WAF Resource file: $OutputFileName"
